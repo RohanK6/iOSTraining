3e1d092e4f9e60c3270119caf0bcf91ff75c72eb
RohanK6 <rohan.khanderia@gmail.com>
2020-12-24 19:22:32 -0500

    Project 3

---
+++ Day 6/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Rohan K on 12/24/20.
+//
+
+import SwiftUI
+
+struct CapsuleText: View {
+    var text: String
+
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+                .foregroundColor(.white)
+            CapsuleText(text: "Second")
+                .foregroundColor(.yellow)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 6/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Rohan K on 12/24/20.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

70b3693edc59ff4b2d4421850a6aefa5e277ff30
RohanK6 <rohan.khanderia@gmail.com>
2020-12-30 01:48:22 -0500

    iExpenses

---
+++ Day 9/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Rohan K on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
